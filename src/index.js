95f0315f8d20d235e502b371143799001cf54187
potapovDim <dmytrop@weblium.com>
2017-06-02 19:52:59 +0300

    hash

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 
 class App extends Component {
+  state = {
+    hash: 1
+  }
+  addNewHash = () => {
+    window.location.hash = this.state.hash + 1
+    this.setState({
+      hash: this.state.hash + 1
+    })
+  }
   render () {
-    return <div>bolerplate</div>
+    return <div>
+    <button onClick={this.addNewHash}>add hash</button>
+    bolerplate
+    </div>
   }
 }
 
