8ab9f261b1b36fcc9753df01ae98880e6f07c051
potapovDim <dmytrop@weblium.com>
2017-06-02 18:15:46 +0300

    set up boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
+
+class App extends Component {
+  render () {
+    return <div>bolerplate</div>
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
