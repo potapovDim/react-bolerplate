b7b47f277338327d14f6306a6546a2abb3601254
potapovDim <dmytrop@weblium.com>
2017-06-06 16:17:25 +0300

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const SvgStore = require('webpack-svgstore-plugin');
 
 module.exports = {
   entry: path.resolve('./src/index.js'),
@@ -7,7 +9,7 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    port: 7070,
+    port: 8082,
     historyApiFallback: true
   },
   module: {
@@ -19,7 +21,55 @@ module.exports = {
         query: {
           presets: ['react', 'es2015', 'stage-0'],
         }
+      }, {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+         fallback: 'style-loader',
+         use: [{
+           loader: 'css-loader',
+           options: {
+             importLoaders: 1,
+             modules: false,
+             sourceMap: true
+           }
+         }, {
+           loader: 'postcss-loader',
+           options: {
+             sourceMap: true
+           }
+         }, {
+           loader: 'sass-loader',
+           options: {
+             sourceMap: true
+           }
+         }]
+      })
+    }, {
+      test: /\.(jpe?g|png|gif)$/i,
+      loader: 'file-loader',
+      query: {
+        name: 'images/[name]-[hash].[ext]?[hash]'
       }
-    ]
-  }
+    }, {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+    }, {
+      test: /\.(eot|ttf|woff|woff2)/,
+      loader: 'file-loader',
+      query: {
+        name: 'fonts/[name].[ext]?[hash]'
+      }
+    }
+  ]},
+  plugins: [
+    new ExtractTextPlugin({filename: 'style.css', allChunks: true}),
+    new SvgStore({
+      svgoOptions: {
+        plugins: [
+          { removeTitle: true }
+        ]
+      },
+      prefix: 'icon'
+    })
+  ]
 }
