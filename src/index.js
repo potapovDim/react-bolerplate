2aa32e36cd5ff3f7700c1e332a96c76e4ba55e69
potapovDim <potapovd3012@gmail.com>
2018-10-06 13:33:35 +0300

    up deps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
-import {Provider} from 'react-redux'
+import Dropzone from 'react-dropzone'
+// import {Provider} from 'react-redux'
 
 
-import MainPage from './main-page'
+class Basic extends React.Component {
+  constructor() {
+    super()
+    this.state = {files: []}
+  }
 
-ReactDom.render(<MainPage />, document.getElementById('app'))
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="dropzone">
+          <Dropzone onDrop={this.onDrop.bind(this)}>
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {
+              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+        </aside>
+      </section>
+    );
+  }
+}
+
+
+
+// import MainPage from './main-page'
+
+ReactDom.render(<Basic />, document.getElementById('app'))
