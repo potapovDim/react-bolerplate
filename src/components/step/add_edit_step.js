f2b60e6059d3418df4c752a21180e3c9c01de00f
potapovDim <potapovd3012@gmail.com>
2018-12-13 14:24:25 +0200

    add logic for change step

--- src/components/step/add_edit_step.js
+++ src/components/step/add_edit_step.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react'
-
+import _ from 'lodash'
 class AddEditStep extends Component {
 
+  changeResult = ({target: {value}}) => {
+    const {updateStep, attachments, title} = this.props
+    _.debounce(() => updateStep({result: value, attachments, title}), 1000)
+  }
+
+  changeTitle = ({target: {value}}) => {
+    const {updateStep, result, attachments} = this.props
+    _.debounce(() => updateStep({result, attachments, title: value}), 1000)
+  }
   render() {
     const {title, result, attachments} = this.props
     return (<div>
