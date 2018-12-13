38985b79db0bdf2afb010fc1e803b3fe35df66c6
potapovDim <potapovd3012@gmail.com>
2018-12-13 19:20:48 +0200

    need backend and refactor current reducers

--- src/components/step/view_step.js
+++ src/components/step/view_step.js
@@ -1,5 +1,13 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 const ViewStep = ({title, result, attachments}) => {
-
+  return (
+    <div>
+      <div>{title}</div>
+      {result && <div>{title}</div>}
+      {attachments.length}
+    </div>
+  )
 }
+
+export {ViewStep}
